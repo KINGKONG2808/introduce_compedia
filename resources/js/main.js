a9a24c49caba058cfe0fdf678ed591c01ae47cbc
HungVV <hungvv@compedia.vn>
2020-11-12 19:09:43 +0700

    update index and css

--- resources/js/main.js
+++ resources/js/main.js
@@ -32,10 +32,32 @@ $(function() {
 function searchClick(e) {
     if (e === true) {
         $('.search-function').hide();
+
+        if ($(window).width() >= 1900) {
+            $('.nav').addClass('display-none');
+            $('.navbar-brand').addClass('display-none');
+            $('.collapse').addClass('padding-1rem');
+            $('.form-inline').css('width', '100%');
+            $('.form-inline').css('left', '34rem');
+            $('.search-function__input').css('width', '100%');
+            $('input').css('width', '49%');
+        }
+
         $('.search-function__input').show();
         $('input').focus();
     } else {
         $('.search-function').show();
+
+        if ($(window).width() >= 1900) {
+            $('.nav').removeClass('display-none');
+            $('.navbar-brand').removeClass('display-none');
+            $('.collapse').removeClass('padding-1rem');
+            $('.form-inline').css('width', '');
+            $('.form-inline').css('left', '');
+            $('.search-function__input').css('width', '');
+            $('input').css('width', '');
+        }
+
         $('.search-function__input').hide();
     }
 }
